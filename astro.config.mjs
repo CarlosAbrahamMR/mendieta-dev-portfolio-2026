4170f468bde6fb2bc1586a07862969283a3175e5
CarlosAbrahamMR <carlos.abraham2000@gmail.com>
2026-03-16 18:32:07 -0600

    add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://TU-USUARIO.github.io",
+  site: "https:///CarlosAbrahamMR.github.io",
   base: "/portfolio",
   vite: {
     plugins: [tailwindcss()]
